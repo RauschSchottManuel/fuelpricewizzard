45ac8f1a1364803e52dd725890294c9e8a955863
Manuel Rausch-Schott <manuel.rausch.schott@gmail.com>
2025-01-17 23:59:33 +0100

    Add responsive layout basics

--- FuelPriceWizard.UI/fuelpricewizard/src/app/app.component.ts
+++ FuelPriceWizard.UI/fuelpricewizard/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OsmMapComponent, OsmMapMarker } from "./shared/osm-map/osm-map.component";
+import { FilterSidebarComponent } from "./core/filter-sidebar/filter-sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, OsmMapComponent],
+  imports: [RouterOutlet, OsmMapComponent, FilterSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
